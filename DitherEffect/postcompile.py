303c61aa75ff7cbc4cf29eca435b36a0e63f24be
Lord Wolfenstein <lord.wolfenstein@power-up.software>
2024-12-04 23:26:22 +0100

    Added EGA palettes.

--- DitherEffect/postcompile.py
+++ DitherEffect/postcompile.py
@@ -6,9 +6,12 @@ dll_file = argv[1]
 key = OpenKey(HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders")
 my_documents, _ = QueryValueEx(key, "Personal")
 CloseKey(key)
-detination_folder = my_documents + "\\paint.net App Files\\Effects"
-copy(dll_file, detination_folder)
+effects_folder = my_documents + "\\paint.net App Files\\Effects"
+palette_folder = my_documents + "\\paint.net App Files\\Palettes"
+copy(dll_file, effects_folder)
+copy("./Basic EGA.txt", palette_folder)
+copy("./Full EGA.txt", palette_folder)
 
 print()
-print(f"Successfully copied {dll_file} to {detination_folder}")
+print(f"Successfully copied {dll_file} to {effects_folder} and EGA palettes to {palette_folder}")
 print()
